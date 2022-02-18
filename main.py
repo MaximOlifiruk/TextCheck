7e3974cf3f3051d3cbf12367e2ba1363e9a4d945
MaximOlifiruk <maxim.olifiruk@gmail.com>
2022-02-18 12:17:46 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,56 @@
+from tkinter import *
+from model import *
+
+
+def func(string):
+    label_choice.config(text=string)
+    if label_choice["text"] == "Выбрана тема про котов":
+        create_data("catsMain.txt")
+    elif label_choice["text"] == "Выбрана тема про собак":
+        create_data("dogsMain.txt")
+    elif label_choice["text"] == "Выбрана тема про кроликов":
+        create_data("rabbitsMain.txt")
+
+
+root = Tk()
+root.title("GUI на Python")
+root["bg"] = "#d0b084"
+root.geometry("800x700+350+100")
+
+main_label = Label(text="Это приложение проверяет как текст написанный в рамке соотносится с темой."
+                   "\nВыберите любую из предложенных тем.", fg="black", bg="#d0b084", font="Arial 14")
+main_label.pack()
+
+label_choice = Label(text="Ничего не выбрано", fg="black", bg="#d0b084", font="Arial 14")
+label_choice.pack()
+
+btn_cats = Button(text="Тема: Коты", background="#ffa26a", foreground="black",
+                  padx="60", pady="10", font="16", command=lambda: func("Выбрана тема про котов"))
+btn_cats.place(x=15, y=100)
+
+btn_dogs = Button(text="Тема: Собаки", background="#ffa26a", foreground="black",
+                  padx="60", pady="10", font="16", command=lambda: func("Выбрана тема про собак"))
+btn_dogs.place(x=255, y=100)
+
+btn_rabbits = Button(text="Тема: Кролики", background="#ffa26a", foreground="black",
+                     padx="60", pady="10", font="16", command=lambda: func("Выбрана тема про кроликов"))
+btn_rabbits.place(x=515, y=100)
+
+label_text = Label(text="Введите текст в форму для сравнения: \n (Берётся из папки ForComparison) ", fg="black", bg="#d0b084",
+                   font="Arial 14")
+label_text.place(relx=0.5, rely=0.3, anchor='center')
+
+text = Text(width=90, height=15)
+text.place(relx=0.5, rely=0.53, anchor='center')
+
+itog = Button(text="Проверить", background="#ffa26a", foreground="black",
+              padx="70", pady="10", font="16", command=lambda: find_accuracy(text, label_accuracy))
+itog.place(x=280, y=550)
+
+label_accuracy = Label(text="Точность равна: ", fg="black", bg="#d0b084", font="Arial 14")
+label_accuracy.place(relx = 0.5,
+                   rely = 0.9,
+                   anchor = 'center')
+
+root.mainloop()
+
---
+++ model.py
@@ -0,0 +1,98 @@
+import pandas as pd
+import regex as re
+from nltk.tokenize import RegexpTokenizer
+
+
+def print_data(data):
+    for elem in data:
+        print(elem)
+
+
+def length_string(string):
+    if len(string) > 30:
+        return True
+    else:
+        return False
+
+
+def cleaning_data(fileName):
+    data = open(fileName, 'r').read()
+    data = cleaning_check_data(data)
+    return data
+
+
+def cleaning_check_data(data):
+    data = re.sub(r"\[\d+\]", "", data)
+    data = re.sub(r"\([А-Яа-я ,.'A-Za-z0-9]+\)", "", data)
+    data = re.sub(r"[,!?@\'\`\"\_\n«»:;()]", "", data)
+    data = re.sub(r"[-—]", " ", data)
+    data = data.split(".")
+    data = list(filter(length_string, data))
+    return data
+
+
+def create_dataframe_check(main, notMain):
+    return pd.DataFrame({
+        'text': main + notMain,
+        'class_label': [1 for i in range(0, len(main))] + [0 for i in range(0, len(notMain))]
+    })
+
+
+def create_dataframe_main(main):
+    return pd.DataFrame({
+        'text': main,
+        'class_label': [1 for i in range(0, len(main))]
+    })
+
+
+def tokenize(data):
+    tokenizer = RegexpTokenizer(r'\w+')
+    data["tokens"] = data["text"].apply(tokenizer.tokenize)
+
+
+def create_data(fileName):
+    dataMain = cleaning_data(fileName)
+    notMain = cleaning_data("notMain.txt")
+    global clean_data
+    clean_data = create_dataframe_check(dataMain, notMain)
+    tokenize(clean_data)
+
+
+def give_text(obj):
+    data_check = cleaning_check_data(str(obj.get("1.0", "end-1c")))
+    global clean_data_check
+    clean_data_check = create_dataframe_main(data_check)
+    tokenize(clean_data_check)
+
+
+def find_accuracy(obj, label_accuracy):
+    give_text(obj)
+    text = pd.concat([clean_data, clean_data_check], axis=0)
+
+    from sklearn.feature_extraction.text import CountVectorizer
+    text_vec = CountVectorizer().fit_transform(text['text'])
+
+    from sklearn.model_selection import train_test_split
+    X_train, X_test, y_train, y_test = train_test_split(text_vec, text['class_label'],
+                                                        test_size=clean_data_check.size / (clean_data_check.size + clean_data.size)
+                                                        , random_state=0, shuffle=False)
+
+    from sklearn import ensemble
+    classifier = ensemble.GradientBoostingClassifier(
+        n_estimators=100,
+        learning_rate=0.5,
+        max_depth=500,
+    )
+
+    classifier.fit(X_train, y_train)
+    predictions = classifier.predict(X_test)
+
+    from sklearn import metrics
+    label_accuracy["text"] = "Точность: " + str(metrics.accuracy_score(y_test, predictions))
+
+
+
+
+
+
+
